109420634899ad2599447963ac43fd645acc999f
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-06-06 15:16:00 -0300

    Project created

---
+++ src/test/java/com/educandoweb2/course2/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb2.course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
