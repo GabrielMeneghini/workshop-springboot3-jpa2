664adff2669755a531002b2390571c1627c7b4b0
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-06-12 21:35:18 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb2/course2/entities/Order.java
+++ src/main/java/com/educandoweb2/course2/entities/Order.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb2.course2.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,6 +28,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone="GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	// Associations
 	@ManyToOne
 	@JoinColumn(name="client_id")
@@ -37,9 +39,10 @@ public class Order implements Serializable {
 	public Order() {	
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -60,6 +63,16 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+	
 	public User getClient() {
 		return client;
 	}
