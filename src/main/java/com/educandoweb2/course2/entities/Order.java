e5286cf06c4c4d9c1293deb756320cf74a7b5f6a
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-06-18 11:34:20 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb2/course2/entities/Order.java
+++ src/main/java/com/educandoweb2/course2/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.educandoweb2.course2.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,9 +39,10 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
-	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	@OneToOne(mappedBy="order", cascade=CascadeType.ALL)
+	private Payment payment;
 	
 	// Constructors
 	public Order() {	
@@ -90,6 +93,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+	
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	// Hashcode and Equals
 	@Override
@@ -108,7 +119,4 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
