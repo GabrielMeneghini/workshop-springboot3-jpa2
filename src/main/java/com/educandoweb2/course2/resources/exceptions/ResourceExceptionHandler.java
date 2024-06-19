1dd8a41aba684cc312f45466b5c425d3bd6446ca
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-06-19 16:43:14 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb2/course2/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.educandoweb2.course2.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.educandoweb2.course2.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/educandoweb2/course2/services/UserService.java
+++ src/main/java/com/educandoweb2/course2/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb2.course2.entities.User;
 import com.educandoweb2.course2.repositories.UserRepository;
+import com.educandoweb2.course2.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
